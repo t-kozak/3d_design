613258ef4912210f1832973f0139f3c4b3dd87b9
Tadeusz <tedk@hey.com>
2025-11-24 19:54:54 +1100

    more work on iphone case

--- src/designs/iphone_case/case.py
+++ src/designs/iphone_case/case.py
@@ -1,14 +1,95 @@
-from cadquery import Assembly, Color
+from cadquery import Assembly, BoundBox, Color, Plane
+from ocp_vscode import show
 
 from designs.iphone_case.iphone import IPhoneDims, IPhones
 from dtools.primitives.transforms import align
 from dtools.texture.hex_grid import HexGridTexture
+from dtools.texture.linear import LinearTexture
 from dtools.workplane import Workplane
+
+
+def _create_phone_mould(iphone: IPhoneDims) -> tuple[Workplane, BoundBox]:
+    phone_hole = iphone.get_body_outline().extrude(iphone_dims.thickness * 1.01)
+    phone_outer_hole = iphone.get_body_outline(scale_x=0.93, scale_y=0.97).extrude(
+        iphone_dims.thickness * 5
+    )
+    phone_hole, phone_outer_hole = align(
+        phone_hole, phone_outer_hole, alignments=("center", "center", "start")
+    )
+    phone_hole = phone_hole.translate((0, 0, 3))
+    phone_outer_hole = phone_outer_hole.translate((0, 0, 3))
+
+    phone_hole_b_box = phone_hole.get_bbox()
+
+    cut_out_body = phone_hole + phone_outer_hole
+
+    for btn in iphone_dims.buttons_cfg:
+        origin_x = 0
+        origin_y = phone_hole_b_box.ymax - btn.top_offset
+        origin_z = phone_hole_b_box.center.z + 1.35  # no idea why
+        if btn.left:
+            origin_x = phone_hole_b_box.xmin
+            normal = (-1, 0, 0)
+
+        else:
+            origin_x = phone_hole_b_box.xmax
+            normal = (1, 0, 0)
+
+        cut_out_body += (
+            Workplane(
+                Plane(
+                    origin=(origin_x, origin_y, origin_z), normal=normal, xDir=(0, 1, 0)
+                )
+            )
+            .rrect(
+                btn.length + 1,
+                btn.width + 1,
+                (btn.width + 1) / 2.01,
+            )
+            .workplane(offset=5)
+            .rrect(
+                btn.length + 4,
+                btn.width + 4,
+                (btn.width + 4) / 2.01,
+            )
+            .loft()
+        )
+
+    # show(cut_out_body)
+    # raise ValueError()
+    return cut_out_body, phone_hole_b_box
+
+
+def _apply_camera_island(
+    body: Workplane, phone_hole_b_box: BoundBox, back_wall_thickness: float
+) -> Workplane:
+    # Add a cutout for the camera island
+    b_box = body.get_bbox()
+    island_center = (24, b_box.ymax - 27)
+    island_outer_outline = (
+        Workplane("XY")
+        .moveTo(*island_center)
+        .rrect(45, 45, 12)
+        .extrude(back_wall_thickness)
+    )
+    island_inner_outline = (
+        Workplane("XY")
+        .moveTo(*island_center)
+        .rrect(43, 43, 12)
+        .extrude(back_wall_thickness)
+    )
+
+    island_wall = island_outer_outline - island_inner_outline
+
+    body -= island_outer_outline
+    body += island_wall
+    return body
 
 
 def create_iphone_case(iphone: IPhoneDims) -> Workplane:
     back_wall_thickness = 3.0
 
+    mould, phone_hole_b_box = _create_phone_mould(iphone)
     # Create main body
     body = (
         iphone.get_body_outline(scale_x=1.1, scale_y=1.05)
@@ -22,51 +103,16 @@ def create_iphone_case(iphone: IPhoneDims) -> Workplane:
                 solid_edge=True,
             )
         )
+        .faces(">Z")
+        .texture(LinearTexture(height=1))
         .translate((0, 0, back_wall_thickness))
     )
 
-    phone_hole = iphone.get_body_outline().extrude(iphone_dims.thickness * 1.01)
-    phone_outer_hole = iphone.get_body_outline(scale_x=0.93, scale_y=0.97).extrude(
-        iphone_dims.thickness * 5
-    )
-    body, phone_hole, phone_outer_hole = align(
-        body, phone_hole, phone_outer_hole, alignments=("center", "center", "start")
-    )
-    phone_hole = phone_hole.translate((0, 0, 3))
-    phone_outer_hole = phone_outer_hole.translate((0, 0, 3))
+    body, mould = align(body, mould, alignments=("center", "center", "start"))
+    mould = mould.translate((0, 0, back_wall_thickness))
+    body -= mould
 
-    phone_hole_b_box = phone_hole.get_bbox()
-
-    body -= phone_hole
-    body -= phone_outer_hole
-
-    # Add a cutout for the camera island
-    island_outer_scale = 1.015
-    island_inner_scale = 1.0
-    island_center = iphone_dims.get_cam_island_center(
-        scale_x=island_outer_scale,
-        scale_y=island_outer_scale,
-        offset=(phone_hole_b_box.xmin, phone_hole_b_box.ymin),
-    )
-    island_center = (island_center[0], island_center[1] - 2)
-    island_outer_outline = (
-        iphone_dims.get_cam_island_base_outline(
-            scale_x=island_outer_scale, scale_y=island_outer_scale
-        )
-        .extrude(back_wall_thickness)
-        .move_center_to(island_center)
-    )
-    island_inner_outline = (
-        iphone_dims.get_cam_island_base_outline(
-            scale_x=island_inner_scale, scale_y=island_inner_scale
-        )
-        .extrude(back_wall_thickness)
-        .move_center_to(island_center)
-    )
-    island_wall = island_outer_outline - island_inner_outline
-
-    body -= island_outer_outline
-    body += island_wall
+    body = _apply_camera_island(body, phone_hole_b_box, back_wall_thickness)
 
     # Add a cutout for ports and microphones and for easy access to navigation
     # (app switcher gesture)
@@ -80,7 +126,7 @@ def create_iphone_case(iphone: IPhoneDims) -> Workplane:
         .workplane(offset=5)
         .moveTo(body_bbox.center.x, 0)
         .rect(39, 20)
-        .workplane(offset=5)
+        .workplane(offset=15)
         .moveTo(body_bbox.center.x, 0)
         .rect(42, 20)
         .loft()
@@ -94,7 +140,7 @@ def create_iphone_case(iphone: IPhoneDims) -> Workplane:
         .workplane(offset=5)
         .moveTo(body_bbox.center.x, body_bbox.ymax)
         .rect(14, 20)
-        .workplane(offset=5)
+        .workplane(offset=15)
         .moveTo(body_bbox.center.x, body_bbox.ymax)
         .rect(20, 20)
         .loft()
@@ -106,11 +152,23 @@ def create_iphone_case(iphone: IPhoneDims) -> Workplane:
     return body
 
 
+# def create_iphone_support_frame(iphone: IPhoneDims):
+#     main_body = iphone.get_body_outline().extrude(iphone.thickness)
+#     bbox = main_body.get_bbox()
+#     hole_dims = (bbox.xlen * 0.85, bbox.ylen * 0.9)
+#     hole = Workplane("XY").rect(*hole_dims).extrude(iphone.thickness)
+#     main_body, hole = align(main_body, hole, alignments=("center", "center", "center"))
+#     main_body -= hole
+#     return main_body
+
+
 if __name__ == "__main__":
     from ocp_vscode import show
 
+    assert show is not None
     iphone_dims = IPhones.IPHONE_16_PRO
 
+    # frame = create_iphone_support_frame(iphone_dims)
     i_case = create_iphone_case(iphone_dims)
 
     phone = iphone_dims.create()
@@ -134,4 +192,19 @@ if __name__ == "__main__":
     )
 
     i_case.export("build/iphone_case.stl")
+
+    case_sampler_box = Workplane("XY").box(20, 20, 50)
+
     show(ass)
+
+    # i_case, case_sampler_box = align(
+    #     i_case, case_sampler_box, alignments=("end", "end", "start")
+    # )
+    # case_sampler = i_case.intersect(case_sampler_box)
+    # frame, case_sampler_box = align(
+    #     frame, case_sampler_box, alignments=("end", "end", "start")
+    # )
+    # frame_sampler = frame.intersect(case_sampler_box)
+
+    # case_sampler.export("build/iphone_case/case_sampler.stl")
+    # frame_sampler.export("build/iphone_case/frame_sampler.stl")
